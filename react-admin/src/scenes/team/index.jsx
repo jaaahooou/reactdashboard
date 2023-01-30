cd4d7c590056649ebe8cdfe772eeb1a63d9b86a1
Jacek Pilch <jaaahooou@wp.pl>
2023-01-30 08:06:27 +0100

    sidebar

---
+++ react-admin/src/scenes/team/index.jsx
@@ -0,0 +1,40 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
+
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+  ];
+  return (
+    <Box>
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box>
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Team;
